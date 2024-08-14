1accf70b9d31a0527f5c54df5fa6cef547ec60c8
EliotVonEcklie <eliotvonecklie@gmail.com>
2024-08-13 21:51:13 -0500

    Add compile options for optimization

--- src/main.rs
+++ src/main.rs
@@ -7,20 +7,25 @@ struct Player;
 
 fn setup(
     mut commands: Commands,
-    mut images: ResMut<Assets<Image>>,
-    asset_server: Res<AssetServer>,
+    asset_server: Res<AssetServer>
 ) {
     commands.spawn(Camera2dBundle::default());
 
-    commands.spawn(SpriteBundle {
-        texture: asset_server.load("textures/floor0.png"),
-        transform: Transform {
-            translation: Vec3::new(0.0, 0.0, 0.0),
-            scale: Vec3::new(1.0, 1.0, 1.0),
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("textures/floor0.png"),
+            transform: Transform {
+                scale: Vec3::new(100.0, 100.0, 0.0),
+                ..default()
+            },
             ..default()
         },
-        ..default()
-    });
+        ImageScaleMode::Tiled {
+            tile_x: true,
+            tile_y: true,
+            stretch_value: 100.0
+        }
+    ));
 
     commands.spawn((
         SpriteBundle {
@@ -31,9 +36,52 @@ fn setup(
     ));
 }
 
+fn update(
+    mut query: Query<&mut Transform, With<Player>>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    time: Res<Time>
+) {
+    let mut transform = query.single_mut();
+
+    let speed = 64.0 * time.delta_seconds(); // 64 pixels * seconds since last frame = 10 pixels per second
+
+    for keycode in keyboard_input.get_pressed() {
+        match keycode {
+            KeyCode::KeyW => {
+                transform.translation.y += speed;
+            },
+            KeyCode::KeyA => {
+                transform.translation.x -= speed;
+            },
+            KeyCode::KeyS => {
+                transform.translation.y -= speed;
+            },
+            KeyCode::KeyD => {
+                transform.translation.x += speed;
+            },
+            _ => {}
+        }
+    }
+}
+
+fn spawn_npc_test (mut event: EventWriter<npc::SpawnNpcEvent>) {
+    event.send(npc::SpawnNpcEvent {
+        transform: Transform {
+            translation: Vec3::new(0.0, 100.0, 1.0),
+            ..default()
+        }
+    });
+}
+
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
+        .add_plugins(DefaultPlugins
+            .set(ImagePlugin::default_nearest())
+        )
+        .add_event::<npc::SpawnNpcEvent>()
         .add_systems(Startup, setup)
+        .add_systems(PostStartup, spawn_npc_test)
+        .add_systems(Update, update)
+        .add_systems(PostUpdate, npc::spawn_npc_event_system)
         .run();
 }
--- src/npc.rs
+++ src/npc.rs
@@ -4,18 +4,23 @@ use bevy::prelude::*;
 pub struct Npc;
 
 #[derive(Event)]
-pub struct SpawnNpcEvent;
+pub struct SpawnNpcEvent {
+    pub transform: Transform
+}
 
-pub fn spawn_npc(
-    evt: EventReader<SpawnNpcEvent>,
+pub fn spawn_npc_event_system(
+    mut events: EventReader<SpawnNpcEvent>,
     mut commands: Commands,
     asset_server: Res<AssetServer>
 ) {
-    commands.spawn((
-        SpriteBundle {
-            texture: asset_server.load("sprites/npc0.png"),
-            ..default()
-        },
-        Npc
-    ));
+    for evt in events.read() {
+        commands.spawn((
+            SpriteBundle {
+                texture: asset_server.load("sprites/npc0.png"),
+                transform: evt.transform,
+                ..default()
+            },
+            Npc
+        ));
+    }
 }
